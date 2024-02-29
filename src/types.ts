2473252cd1b0db29ef37a576c83821e8ab071596
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-02-29 10:53:38 +0100

    Add types file with types

---
+++ src/types.ts
@@ -0,0 +1,49 @@
+export interface GeneralVideo {
+  id: ID
+  url: string
+  title: string
+  channelName: string
+  description: string
+  durationRaw: string
+  snippet: Snippet
+  views: string
+}
+
+interface ID {
+  videoID: string
+}
+
+interface Snippet {
+  url: string
+  duration: string
+  publishedAt: string
+  thumbnails: Thumbnails
+  title: string
+  views: string
+}
+
+interface Thumbnails {
+  id: string
+  url: string
+  height: number
+  width: number
+}
+
+export interface Video {
+  videoId: string
+  url: string
+  title: string
+  description: string
+  owner: string
+  channelId: string
+  thumbnailUrl: string
+  datePublished: Date
+  genre: string
+  paid: boolean
+  unlisted: boolean
+  isFamilyFriendly: boolean
+  duration: number
+  views: number
+  regionsAllowed: string[]
+  likeCount: number
+}
