d1696733f213ca2835ac6995fd68c42d1bc3e470
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-02-29 10:54:04 +0100

    WIP: Video details design

--- src/routes/VideoDetails.tsx
+++ src/routes/VideoDetails.tsx
@@ -1,3 +1,43 @@
+import styled from 'styled-components'
+import { useParams } from 'react-router-dom'
+import { fetcher } from '../utils'
+import useSWR from 'swr'
+import { Video } from '../types'
+
+const Page = styled.section``
+
+const Error = styled.strong`
+  padding: 50px;
+  font-size: 32px;
+`
+
 export const VideoDetails = () => {
-  return <h1>Video Details</h1>
+  const { id } = useParams()
+  const {
+    data: video,
+    error,
+    isLoading,
+  } = useSWR<Video>(
+    `https://youtube.thorsteinsson.is/api/videos/${id}`,
+    fetcher,
+  )
+
+  if (isLoading) return <div>Loading...</div>
+  if (error || video === undefined) return <Error>Error loading videos</Error>
+
+  console.log(video)
+
+  return (
+    <Page>
+      <iframe
+        width="1000"
+        height="561"
+        src={`https://www.youtube.com/embed/${id}`}
+        title={video.title}
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowFullScreen
+      />
+      <h1>{video.title}</h1>
+    </Page>
+  )
 }
