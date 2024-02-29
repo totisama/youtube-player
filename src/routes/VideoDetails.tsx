3cc45f5483d6608eb8248dfca0928cb85dcae0d6
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-02-29 12:03:07 +0100

    tweak style of page

--- src/routes/VideoDetails.tsx
+++ src/routes/VideoDetails.tsx
@@ -1,10 +1,48 @@
 import styled from 'styled-components'
 import { useParams } from 'react-router-dom'
-import { fetcher } from '../utils'
+import { fetcher, formatViews } from '../utils'
 import useSWR from 'swr'
 import { Video } from '../types'
 
-const Page = styled.section``
+const Page = styled.main`
+  margin-top: 25px;
+  padding: 0px 50px;
+  color: white;
+  padding-bottom: 100px;
+`
+
+const IFrame = styled.iframe`
+  border: 0;
+`
+
+const TitleInfo = styled.div`
+  display: flex;
+  align-items: end;
+  margin: 10px 0;
+  gap: 10px;
+`
+
+const Title = styled.h1`
+  font-size: 28px;
+  margin: 0px;
+`
+
+const Information = styled.section`
+  background-color: #1c1c1c;
+  padding: 5px 0 20px 15px;
+  margin-top: 10px;
+  width: 80%;
+`
+
+const ChannelName = styled.h2`
+  color: #dedede;
+  font-size: 20px;
+`
+
+const Description = styled.p`
+  color: #dedede;
+  font-size: 18px;
+`
 
 const Error = styled.strong`
   padding: 50px;
@@ -22,22 +60,30 @@ export const VideoDetails = () => {
     fetcher,
   )
 
-  if (isLoading) return <div>Loading...</div>
-  if (error || video === undefined) return <Error>Error loading videos</Error>
+  // console.log(video)
 
-  console.log(video)
+  if (isLoading) return <div>Loading...</div>
+  if (error || video === undefined) return <Error>Error loading video</Error>
 
   return (
     <Page>
-      <iframe
-        width="1000"
-        height="561"
-        src={`https://www.youtube.com/embed/${id}`}
+      <IFrame
+        width="1100"
+        height="615"
+        src={`https://www.youtube.com/embed/${id}?autoplay=1`}
         title={video.title}
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        frameBorder={0}
         allowFullScreen
       />
-      <h1>{video.title}</h1>
+      <Information>
+        <TitleInfo>
+          <Title>{video.title}</Title>
+          {formatViews(String(video.views))} views
+        </TitleInfo>
+        <ChannelName>{video.owner}</ChannelName>
+        <Description>{video.description}</Description>
+        {new Date(video.datePublished).toDateString()}
+      </Information>
     </Page>
   )
 }
