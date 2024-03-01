4db3458ca78905c5a611fa0ca297d4458526e9fb
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-03-01 11:29:50 +0100

    create YoutubeVideo Component

---
+++ src/components/YoutubeVideo.tsx
@@ -0,0 +1,42 @@
+import { CurrentVideoContextType, YouTubeVideoProps } from '../types'
+import YouTube, { type YouTubeProps } from 'react-youtube'
+import { useContext, useRef } from 'react'
+import { CurrentVideoContext } from '../context/CurrentVideo'
+
+export const YoutubeVideo = ({ videoId, width, height }: YouTubeVideoProps) => {
+  const { currentSeconds, setCurrentSeconds } = useContext(
+    CurrentVideoContext,
+  ) as CurrentVideoContextType
+  const starSeconds = useRef(currentSeconds)
+  let intervalId: ReturnType<typeof setInterval> | null = null
+
+  const opts: YouTubeProps['opts'] = {
+    width: width,
+    height: height,
+    playerVars: {
+      autoplay: 1,
+      start: starSeconds.current,
+    },
+  }
+
+  const onPlay: YouTubeProps['onPlay'] = (event) => {
+    intervalId = setInterval(() => {
+      setCurrentSeconds(Math.floor(event.target.getCurrentTime()))
+    }, 1000)
+  }
+
+  const onPause: YouTubeProps['onPause'] = () => {
+    if (intervalId) clearInterval(intervalId)
+    intervalId = null
+  }
+
+  return (
+    <YouTube
+      key={videoId}
+      videoId={videoId}
+      opts={opts}
+      onPlay={onPlay}
+      onPause={onPause}
+    />
+  )
+}
