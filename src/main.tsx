4513ee41713aade63ba58f4d2f700b8fb10e95bb
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-03-04 10:36:21 +0100

    Create navBar component and change Form style

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,20 @@ import ReactDOM from 'react-dom/client'
 import Home from './routes/Home.tsx'
 import { VideoDetails } from './routes/VideoDetails.tsx'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { SearchForm } from './components/SearchForm.tsx'
-import './index.css'
 import { CurrentVideoProvider } from './context/CurrentVideo.tsx'
+import { Playlists } from './routes/Playlists.tsx'
+import './index.css'
+import { NavBar } from './components/NavBar.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <CurrentVideoProvider>
       <BrowserRouter>
-        <SearchForm />
+        <NavBar />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/detail/:id" element={<VideoDetails />} />
+          <Route path="/playlists/" element={<Playlists />} />
         </Routes>
       </BrowserRouter>
     </CurrentVideoProvider>
