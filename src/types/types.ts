6b632ee3743932085004755172fb369a1ca2b973
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-12-12 18:17:17 +0100

    add types

---
+++ src/types/types.ts
@@ -0,0 +1,35 @@
+export interface Video {
+  id: ID
+  url: string
+  title: string
+  channelName: string
+  description: string
+  duration_raw: string
+  snippet: Snippet
+  views: string
+}
+
+export interface ID {
+  videoId: string
+}
+
+export interface Snippet {
+  url: string
+  duration: string
+  publishedAt: string
+  thumbnails: Thumbnails
+  title: string
+  views: string
+}
+
+export interface Thumbnails {
+  id: string
+  url: string
+  height: number
+  width: number
+}
+
+export interface SearchContextType {
+  search: string | null
+  setSearch: (seconds: string) => void
+}
