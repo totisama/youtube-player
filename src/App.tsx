c1abe8d4789d6693c30f6ffbfc7b03494c423623
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-12-12 18:18:27 +0100

    use provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Routes, Route, BrowserRouter } from 'react-router'
 import { MainLayout } from './components/layouts/MainLayout'
-// import { Home } from './pages/Home'
+import { Home } from './pages/Home'
+import { SearchProvider } from './lib/providers/SearchProvider'
 // import { VideoDetails } from './pages/VideoDetails'
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          {/* <Route index element={<Home />} /> */}
-          {/* <Route path="video/:id" element={<VideoDetails />} /> */}
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <SearchProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<MainLayout />}>
+            <Route index element={<Home />} />
+            {/* <Route path="video/:id" element={<VideoDetails />} /> */}
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </SearchProvider>
   )
 }
