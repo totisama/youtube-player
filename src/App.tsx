2e76c49ba662d52ca7446a18ae1ad439008745a9
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-12-16 19:38:43 +0100

    add new route

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { MainLayout } from './components/layouts/MainLayout'
 import { VideoList } from './pages/VideoList'
 import { CurrentVideoProvider } from './lib/providers/CurrentVideoProvider'
 import { VideoDetails } from './pages/VideoDetails'
+import { Playlists } from './pages/Playlists'
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" element={<MainLayout />}>
             <Route index element={<VideoList />} />
             <Route path="video/:videoId" element={<VideoDetails />} />
+            <Route path="playlists" element={<Playlists />} />
           </Route>
         </Routes>
       </BrowserRouter>
