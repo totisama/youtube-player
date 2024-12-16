cac38c75a6364f378db0df1708283df52a6c56c8
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-12-16 20:11:08 +0100

    add link to navigate to playlists page

--- src/components/layouts/MainLayout.tsx
+++ src/components/layouts/MainLayout.tsx
@@ -2,21 +2,29 @@ import styled from 'styled-components'
 import { Link, Outlet } from 'react-router'
 import { SearchInput } from '../SearchInput'
 import { FloatingPlayer } from '../FloatingPlayer'
+import { ModalProvider } from 'styled-react-modal'
 
 export function MainLayout() {
   return (
-    <LayoutContainer>
-      <Header>
-        <Link to="/">
-          <h1>YouTube Player</h1>
-        </Link>
-        <SearchInput />
-      </Header>
-      <MainContent>
-        <Outlet />
-      </MainContent>
-      <FloatingPlayer />
-    </LayoutContainer>
+    <ModalProvider>
+      <LayoutContainer>
+        <Header>
+          <NavContainer>
+            <Link to="/">
+              <h2>YouTube Player</h2>
+            </Link>
+            <nav>
+              <NavItem to="/playlists">Playlists</NavItem>
+            </nav>
+          </NavContainer>
+          <SearchInput />
+        </Header>
+        <MainContent>
+          <Outlet />
+        </MainContent>
+        <FloatingPlayer />
+      </LayoutContainer>
+    </ModalProvider>
   )
 }
 
@@ -35,6 +43,30 @@ const Header = styled.header`
   display: flex;
   align-items: center;
   justify-content: space-between;
+`
+
+const NavContainer = styled.div`
+  display: flex;
+  align-items: flex-end;
+  gap: 20px;
+`
+
+const NavItem = styled(Link)`
+  color: white;
+  text-decoration: none;
+  font-size: 18px;
+  display: inline-block;
+  transition: transform 0.2s, color 0.3s ease;
+
+  &:hover {
+    text-decoration: underline;
+    transform: scale(1.1) rotate(3deg);
+    color: #ffcc00;
+  }
+
+  &:active {
+    transform: scale(1) rotate(0);
+  }
 `
 
 const MainContent = styled.main`
