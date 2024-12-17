5160fca6455e55c01d07d1a93adf7e4aa8b5cc50
Rodrigo Samayoa <toti1998sama@gmail.com>
2024-12-17 17:42:43 +0100

    add loader component

---
+++ src/components/Loader.tsx
@@ -0,0 +1,42 @@
+import styled, { keyframes } from 'styled-components'
+
+const spin = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`
+
+export const Spinner = styled.div`
+  display: inline-block;
+  height: 2rem;
+  width: 2rem;
+  border: 4px solid currentColor;
+  border-right-color: transparent;
+  border-radius: 50%;
+  animation: ${spin} 1s linear infinite;
+  color: #ffffff
+  position: relative;
+`
+
+export const VisuallyHidden = styled.span`
+  position: absolute;
+  height: 1px;
+  width: 1px;
+  overflow: hidden;
+  white-space: nowrap;
+  border: 0;
+  padding: 0;
+  clip: rect(0, 0, 0, 0);
+  margin: -1px;
+`
+
+export const Loader = () => {
+  return (
+    <Spinner role="status" aria-label="Loading...">
+      <VisuallyHidden>Loading...</VisuallyHidden>
+    </Spinner>
+  )
+}
